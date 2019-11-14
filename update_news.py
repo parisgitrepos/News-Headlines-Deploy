c072255915c9173afe119ff7a776283881867ecb
Paras P <punaterparas@gmail.com>
2019-11-14 22:47:02 +0000

    update_news.py created online with Bitbucket

---
+++ update_news.py
@@ -0,0 +1,20 @@
+import datetime
+
+hour = int(datetime.time().strftime('%H'))
+hour -= 5
+
+while True:
+    if hour == 0:
+        pass
+        # Update variables
+    elif hour == 6:
+        pass
+        # Update variables
+    elif hour == 12:
+        pass
+        # Update variables
+    elif hour == 18:
+        pass
+        # Update variables
+    else:
+        pass
